3168ea3c4f9f484fab7f01c8691ad131771c1a39
Miguel SE <masemmiguel@gmail.com>
2023-04-12 11:35:14 -0400

    advanced searching for the rooms

--- public/js/index.js
+++ public/js/index.js
@@ -18,15 +18,7 @@ function makeBuilding(building, availability, imageURL) {
     elem.href = '/rooms/' + + building.building_id + "/" + building.building_name;
     elem.innerHTML = `<h2 class="searchable">${building.building_name}</h2>
     <p>${content}</p>`;
-    /*
-    if(imageURL) elem.style.backgroundImage = `url('${imageURL}')`;
-    else {
-        getBuildingImage(building.building_name, '').then((img) => {
-            elem.style.backgroundImage = `url('${img}')`;
-        });
-    }
-    */
-   elem.style.backgroundImage = `url('${building.image_url}')`
+    elem.style.backgroundImage = `url('${building.image_url}')`
     elem.onclick = () => {localStorage.setItem('building_id', building.building_id)}
 
     cards.appendChild(elem);
