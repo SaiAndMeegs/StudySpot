d682a3ffbf1b029b55450701a36514fecfa42d68
Sai Sompally <ssompally@gmail.com>
2023-05-02 19:26:07 -0400

    improve code quality

--- heroku_update_room_availability.js
+++ heroku_update_room_availability.js
@@ -11,14 +11,14 @@ client.connect();
 
 client.query(`SET time zone 'America/Montreal';
 
-    UPDATE building_room SET curr_availability = 2;
-    
-    UPDATE building_room SET curr_availability = 0
-        WHERE building_room_id IN 
-        (SELECT course.building_room_id FROM course_meeting course 
-            WHERE array_to_string(course.days, ',') LIKE '%' || TRIM(INITCAP(to_char(now(), 'day'))) || '%' AND
-            TO_TIMESTAMP(to_char(now(), 'HH24:MI:SS'), 'HH24:MI:SS') BETWEEN TO_TIMESTAMP(course.start_time, 'HH24:MI:SS') AND 
-            TO_TIMESTAMP(course.end_time, 'HH24:MI:SS'))`, (err, res) => {
+            UPDATE building_room SET curr_availability = 2;
+
+            UPDATE building_room SET curr_availability = 0
+                WHERE building_room_id IN 
+                (SELECT course.building_room_id FROM course_meeting course 
+                    WHERE array_to_string(course.days, ',') LIKE '%' || TRIM(INITCAP(to_char(now(), 'day'))) || '%' AND
+                    TO_TIMESTAMP(to_char(now(), 'HH24:MI:SS'), 'HH24:MI:SS') BETWEEN TO_TIMESTAMP(course.start_time, 'HH24:MI:SS') AND 
+                    TO_TIMESTAMP(course.end_time, 'HH24:MI:SS'))`, (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
