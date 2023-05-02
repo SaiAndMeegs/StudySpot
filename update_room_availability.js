b7bdc9ce426b80b4ff0507991cb5720442bf6fc7
Sai Sompally <ssompally@gmail.com>
2023-05-02 19:13:51 -0400

    remote connecting to db

--- update_room_availability.js
+++ update_room_availability.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5432;
 const {Pool} = require('pg')
 const axios = require('axios')
 
-/*
+
 const pool = new Pool({
     user: "kiyeotntjhtpfw",
     host: "ec2-34-202-127-5.compute-1.amazonaws.com",
@@ -16,10 +16,30 @@ const pool = new Pool({
     port: `${PORT}`,
     ssl: {
         rejectUnauthorized: false
-      }
+      },
+    sslmode: "require"
 })
-*/
 
+//Set appropriate time zone, make all classes available, then make classrooms with classes in session unavailable
+pool.query(
+    `SET time zone 'America/Montreal';
+
+    UPDATE building_room SET curr_availability = 2;
+    
+    UPDATE building_room SET curr_availability = 0
+        WHERE building_room_id IN 
+        (SELECT course.building_room_id FROM course_meeting course 
+            WHERE array_to_string(course.days, ',') LIKE '%' || TRIM(INITCAP(to_char(now(), 'day'))) || '%' AND
+            TO_TIMESTAMP(to_char(now(), 'HH24:MI:SS'), 'HH24:MI:SS') BETWEEN TO_TIMESTAMP(course.start_time, 'HH24:MI:SS') AND 
+            TO_TIMESTAMP(course.end_time, 'HH24:MI:SS'))`, (err, result) => {
+    
+    if (err) throw err
+
+    pool.end;
+})
+
+
+/*
 const connectDb = async () => {
         try {
             const pool = new Pool({
@@ -52,21 +72,8 @@ const connectDb = async () => {
     }
     
 connectDb()
-    
-/*
-//Set appropriate time zone, make all classes available, then make classrooms with classes in session unavailable
-pool.query(
-    `SET time zone 'America/Montreal';
-
-    UPDATE building_room SET curr_availability = 2;
-    
-    UPDATE building_room SET curr_availability = 0
-        WHERE building_room_id IN 
-        (SELECT course.building_room_id FROM course_meeting course 
-            WHERE array_to_string(course.days, ',') LIKE '%' || TRIM(INITCAP(to_char(now(), 'day'))) || '%' AND
-            TO_TIMESTAMP(to_char(now(), 'HH24:MI:SS'), 'HH24:MI:SS') BETWEEN TO_TIMESTAMP(course.start_time, 'HH24:MI:SS') AND 
-            TO_TIMESTAMP(course.end_time, 'HH24:MI:SS'))`);
 */
+
 
 /*
 //Make classrooms without classes in session as available
