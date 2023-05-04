aeab66fbe0c68b5b27e5e8891d76e23e9ae2aa17
Sai Sompally <ssompally@gmail.com>
2023-05-04 11:34:47 -0400

    storing images in express for improved performance

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const pool = new Pool({
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/images', express.static(__dirname + 'public/images'))
 
 //  Listen
 app.listen(PORT, () => {
