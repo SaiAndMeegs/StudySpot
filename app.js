4442324c2110390d91244c3a01ba3b6a4150b2ac
Sai Sompally <ssompally@gmail.com>
2023-05-03 21:34:13 -0400

    get endpoint debug

--- app.js
+++ app.js
@@ -125,12 +125,12 @@ app.get('/course_meetings/id/:id', cors(), (req, res) => {
 })
 
 //student_event endpoints
-app.get('/student_events/range_of_days/:building_room_id/:start_date/:end_date', cors(), (req, res) => {
+app.get('/student_events/:start_date/:end_date', cors(), (req, res) => {
     let building_room_id = req.params.building_room_id;
     let start_date = req.params.start_date;
     let end_date = req.params.end_date;
 
-    pool.query(`SELECT * FROM student_event WHERE building_room_id = ` + building_room_id ` AND DATE BETWEEN TO_DATE(` + start_date`, 'YYYY MM DD') AND TO_DATE(` + end_date + `, 'YYYY MM DD')`, (err, result) => {        
+    pool.query(`SELECT * FROM student_event WHERE DATE BETWEEN TO_DATE(` + start_date`, 'YYYY.MM.DD') AND TO_DATE(` + end_date + `, 'YYYY.MM.DD')`, (err, result) => {        
         if (err) throw err
         res.status(200).json(result.rows)
   
